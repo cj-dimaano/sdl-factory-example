a34ce3904ed487a6cd8a01aeddbaa216266956fa
CJ Dimaano <c.j.s.dimaano@gmail.com>
2017-07-06 09:47:19 -0700

    add robot machine

---
+++ src/RobotMachine.cpp
@@ -0,0 +1,103 @@
+/*******************************************************************************
+@file `RobotMachine.cpp`
+  Created July 5, 2017
+
+@author CJ Dimaano
+  <c.j.s.dimaano@gmail.com>
+*******************************************************************************/
+
+#include "RobotMachine.h"
+
+#define HAS_TARGET_CHANGED_EVENT "RobotMachine::HasTargetChanged"
+
+static SDL_Point makePoint(int x, int y) {
+  SDL_Point p = {x, y};
+  return p;
+}
+
+static SDL_Rect makeRect(int x, int y, int w, int h) {
+  SDL_Rect r = {x, y, w, h};
+  return r;
+}
+
+static std::vector<SDL_Point> getNeighbors(SDL_Point p) {
+  std::vector<SDL_Point> neighbors;
+  neighbors.push_back(makePoint(p.x, p.y - 1));
+  neighbors.push_back(makePoint(p.x - 1, p.y - 1));
+  neighbors.push_back(makePoint(p.x - 1, p.y));
+  neighbors.push_back(makePoint(p.x - 1, p.y + 1));
+  neighbors.push_back(makePoint(p.x, p.y + 1));
+  neighbors.push_back(makePoint(p.x + 1, p.y + 1));
+  neighbors.push_back(makePoint(p.x + 1, p.y));
+  neighbors.push_back(makePoint(p.x + 1, p.y + 1));
+  return neighbors;
+}
+
+RobotMachine::RobotMachine(SDL_Texture *spritesheet, SDL_Rect drawRegion,
+                           SDL_Point factoryPoint)
+    : Machine(AnimatedSprite(spritesheet, makeRect(0, 48, 32, 16), drawRegion,
+                             16, 16, 2, 100),
+              factoryPoint, 500),
+      _pickTarget(
+          std::bind(&RobotMachine::SetTargetPath, this,
+                    std::pair<StructureMachine *, std::vector<SDL_Point>>()),
+          &getNeighbors),
+      _stepDelay(500), _stepTick(0), _isEmpty(true),
+      _emptySpriteRegion(makeRect(0, 48, 32, 16)),
+      _fullSpriteRegion(makeRect(0, 64, 32, 16)), _target(NULL) {
+  EventEmitter<RobotMachine>::AddEvent(HAS_TARGET_CHANGED_EVENT);
+  EventPayload<Machine> payload(this);
+  AddIsIdleChangedEventHandler(
+      std::bind(&RobotMachine::IsIdleChanged, this, payload));
+}
+
+void RobotMachine::AddHasTargetChangedEventHandler(
+    std::function<void(EventPayload<RobotMachine> &)> handler) {
+  EventEmitter<RobotMachine>::AddEventHandler(HAS_TARGET_CHANGED_EVENT,
+                                              handler);
+}
+
+void RobotMachine::PickTarget(std::vector<StructureMachine *> candidates) {
+  _pickTarget.Begin(GetFactoryPoint(), candidates);
+}
+
+void RobotMachine::OnHasTargetChanged() {
+  EventPayload<RobotMachine> payload(this);
+  EventEmitter<RobotMachine>::EmitEvent(HAS_TARGET_CHANGED_EVENT, payload);
+}
+
+void RobotMachine::OnUpdate(unsigned int dt) {
+  _stepTick = _path.empty() ? 0 : _stepTick + dt;
+  if (_stepTick >= _stepDelay) {
+    _stepTick -= _stepDelay;
+    SetFactoryPoint(_path.back());
+    _path.pop_back();
+    if (_path.empty() && _target != NULL && _target->IsIdle()) {
+      SDL_Point targetPoint = _target->GetFactoryPoint();
+      SDL_Point thisPoint = GetFactoryPoint();
+      if (targetPoint.x == thisPoint.x && targetPoint.y == thisPoint.y)
+        Restart();
+      else {
+        _target = NULL;
+        OnHasTargetChanged();
+      }
+    }
+  }
+}
+
+void RobotMachine::SetTargetPath(
+    std::pair<StructureMachine *, std::vector<SDL_Point>> &targetPath) {
+  _target = targetPath.first;
+  _path = targetPath.second;
+  OnHasTargetChanged();
+}
+
+void RobotMachine::IsIdleChanged(EventPayload<Machine> &) {
+  if (IsIdle()) {
+    _target = NULL;
+    _isEmpty = !_isEmpty;
+    GetMachineSprite().SetSpriteRegion(_isEmpty ? _emptySpriteRegion
+                                                : _fullSpriteRegion);
+    OnHasTargetChanged();
+  }
+}
---
+++ src/RobotMachine.h
@@ -0,0 +1,180 @@
+/*******************************************************************************
+@file `RobotMachine.h`
+  Created June 29, 2017
+
+@author CJ Dimaano
+  <c.j.s.dimaano@gmail.com>
+*******************************************************************************/
+
+#ifndef ROBOTMACHINE_H
+#define ROBOTMACHINE_H
+
+#include "Events.h"
+#include "Machine.h"
+#include "PickTargetAlgorithm.h"
+#include "StructureMachine.h"
+#include <SDL2/SDL.h>
+#include <functional>
+#include <utility>
+#include <vector>
+
+/**
+ * `RobotMachine`
+ *
+ *   Represents a robot machine in a factory.
+ */
+class RobotMachine : public Machine, private EventEmitter<RobotMachine> {
+
+  /**
+   * `_stepDelay`
+   *
+   *   The number of ticks required for the robot to make a full step.
+   */
+  unsigned int _stepDelay;
+
+  /**
+   * `_stepTick`
+   *
+   *   The number of ticks that have passed since the previous step.
+   */
+  unsigned int _stepTick;
+
+  /**
+   * `_isEmpty`
+   *
+   *   Whether or not the robot is carrying a payload.
+   */
+  bool _isEmpty;
+
+  /**
+   * `_emptySpriteRegion`
+   *
+   *   The region of the sprite sheet for the empty robot.
+   */
+  SDL_Rect _emptySpriteRegion;
+
+  /**
+   * `_fullSpriteRegion`
+   *
+   *   The region of the sprite sheet for the loaded robot.
+   */
+  SDL_Rect _fullSpriteRegion;
+
+  /**
+   * `_path`
+   *
+   *   The stack of points representing the path from the robot to the target.
+   */
+  std::vector<SDL_Point> _path;
+
+  /**
+   * `_target`
+   *
+   *   The current target of the robot.
+   */
+  StructureMachine *_target;
+
+  /**
+   * `_pickTarget`
+   *
+   *   Picks a target from a collection of candidates.
+   */
+  PickTargetAlgorithm _pickTarget;
+
+public:
+  /**
+   * `RobotMachine`
+   *
+   *   Constructor.
+   *
+   * @param spritesheet
+   *   The SDL texture containing the sprites.
+   *
+   * @param drawRegion
+   *   The region of the renderer on which to draw the sprite.
+   *
+   * @param factoryPoint
+   *   The factory coordinates of the machine.
+   */
+  RobotMachine(SDL_Texture *spritesheet, SDL_Rect drawRegion, SDL_Point factoryPoint);
+
+  /**
+   * `AddHasTargetChangedEventHandler`
+   *
+   *   Adds an event handler for the `HasTargetChanged` event.
+   */
+  void AddHasTargetChangedEventHandler(
+      std::function<void(EventPayload<RobotMachine> &)> handler);
+
+  /**
+   * `PickTarget`
+   *
+   *   Selects the nearest target from a collection of candidate structure
+   *   machines.
+   *
+   * @description
+   *   The shortest path is calculated for each candidate. The candidate with
+   *   the shortest path is chosen as the target.
+   *
+   *   It is recommended that the factory recall this method if the collection
+   *   of candidates changes.
+   */
+  void PickTarget(std::vector<StructureMachine *> candidates);
+
+  /**
+   * `HasTarget`
+   *
+   *   True if the robot has a target; otherwise, false.
+   */
+  bool HasTarget() { return _target != NULL; }
+
+  /**
+   * `IsEmpty`
+   *
+   *   True if the robot is emptyhanded; otherwise, false.
+   */
+  bool IsEmpty() { return _isEmpty; }
+
+  /**
+   * `GetStepProgress`
+   *
+   *   Gets a value between 0 and 1 indicating how much progress towards the
+   *   next step the robot has made.
+   */
+  double GetStepProgress() {
+    return static_cast<double>(_stepTick) / static_cast<double>(_stepDelay);
+  }
+
+protected:
+  /**
+   * `OnHasTargetChanged`
+   *
+   *   Event when the machine changes its target.
+   */
+  virtual void OnHasTargetChanged();
+
+  /**
+   * `OnUpdate`
+   *
+   *   Event when the machine is being updated.
+   */
+  void OnUpdate(unsigned int dt);
+
+private:
+  /**
+   * `SetTargetPath`
+   *
+   *   Callback for the pick target algorithm.
+   */
+  void SetTargetPath(
+      std::pair<StructureMachine *, std::vector<SDL_Point>> &targetPath);
+
+  /**
+   * `IsIdleChanged`
+   *
+   *   Handles the IsIdleChanged event.
+   */
+  void IsIdleChanged(EventPayload<Machine> &payload);
+};
+
+#endif
